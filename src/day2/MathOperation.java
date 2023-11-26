339289c317857b7d61b79100f9f7b68d6a6eb03c
gaonkarswapnil <31978392+gaonkarswapnil@users.noreply.github.com>
2023-11-26 19:49:22 +0530

    static Class

---
+++ src/day2/MathOperation.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class MathOperation {
+	public static float pi = 3.14f;
+	
+	public static int doAdd(int x, int y) {
+		return x+y;
+	}
+
+	public static int doSub(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+}
---
+++ src/day2/MathOperationMain.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class MathOperationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Value of PI  is: "+MathOperation.pi);
+		System.out.println("Addition of 5 and 10 is: "+MathOperation.doAdd(5, 10));
+		System.out.println("Subtraction of 5 and 10 is: "+MathOperation.doSub(5, 10));
+	}
+
+}
