8adef33a3bfffa818bb2db3e9fc63638deac08fb
gaonkarswapnil <31978392+gaonkarswapnil@users.noreply.github.com>
2023-11-28 19:10:01 +0530

    Date and Time

---
+++ src/day11/DateAndTimeMain.java
@@ -0,0 +1,28 @@
+package day11;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public class DateAndTimeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate date = LocalDate.now();
+		LocalTime time = LocalTime.now();
+		LocalDateTime dateTime = LocalDateTime.now();
+		
+		System.out.println(date);
+		System.out.println(time);
+		System.out.println(dateTime);
+		
+		LocalDate independence = LocalDate.of(1947, 8, 15);
+		System.out.println(independence);
+		
+		LocalDate nextDate = date.plusDays(6);
+		System.out.println(nextDate);
+		
+	}
+
+}
